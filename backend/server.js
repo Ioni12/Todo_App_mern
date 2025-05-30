73b9871bcb3d90fea3abe760f88520edd1328a45
enkeljon gjeta <enkelionimarku@gmail.com>
2025-05-30 18:49:46 +0200

    lets see

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,4 @@ connectDB();
 app.use(express.json());
 app.use("/api/routes", todoRoutes);
 
-app.get("/api/todos", (req, res) => {
-  res.json([{ id: 1, task: "Test todo", completed: false }]);
-});
-
 app.listen(process.env.PORT, () => console.log("server up"));
